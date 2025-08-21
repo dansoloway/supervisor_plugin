557c8b75efce8c1174cc7868a3eef0d1be7e7d9e
Daniel Soloway <dansoloway@gmail.com>
2025-08-22 00:27:06 +0300

    menu on all pages maybe

--- templates/supervisor-home.php
+++ templates/supervisor-home.php
@@ -6,21 +6,12 @@ get_header('supervisor');
 <div class="supervisor-home">
 
     <!-- Navigation Menu -->
-    <nav class="site-nav supervisor_header_links" aria-label="ראשי">
-        <?php
-            $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
-            if (file_exists($nav_path)) {
-                ob_start();
-                require_once $nav_path;
-                $nav_content = ob_get_clean();
-                // Extract just the links from the navigation file
-                preg_match_all('/<a[^>]*>.*?<\/a>/s', $nav_content, $matches);
-                if (!empty($matches[0])) {
-                    echo implode('', $matches[0]);
-                }
-            }
-        ?>
-    </nav>
+    <?php
+        $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
+        if (file_exists($nav_path)) {
+            require_once $nav_path;
+        }
+    ?>
 
     <!-- Introductory Text Block -->
     <div class="intro-text-block">

95eee3309f0c42ac9b3a1daa8ff2bac6bc0525c4
Daniel Soloway <dansoloway@gmail.com>
2025-08-22 00:14:06 +0300

    content page

--- templates/supervisor-content.php
+++ templates/supervisor-content.php
@@ -4,12 +4,21 @@ get_header('supervisor'); ?>
 
 <div class="supervisor-home">
     <!-- Navigation Menu -->
-    <?php
-        $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
-        if (file_exists($nav_path)) {
-            require_once $nav_path;
-        }
-    ?>
+    <nav class="site-nav supervisor_header_links" aria-label="ראשי">
+        <?php
+            $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
+            if (file_exists($nav_path)) {
+                ob_start();
+                require_once $nav_path;
+                $nav_content = ob_get_clean();
+                // Extract just the links from the navigation file
+                preg_match_all('/<a[^>]*>.*?<\/a>/s', $nav_content, $matches);
+                if (!empty($matches[0])) {
+                    echo implode('', $matches[0]);
+                }
+            }
+        ?>
+    </nav>
 
     <div class="supervisor-content supervisor-single-column-content">
         <div class="content-main">

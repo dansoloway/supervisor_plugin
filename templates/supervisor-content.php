604bf05954fd7ddd9f89f652413240e36cbc2d7e
Daniel Soloway <dansoloway@gmail.com>
2025-08-22 00:11:24 +0300

    content page

--- templates/supervisor-content.php
+++ templates/supervisor-content.php
@@ -4,42 +4,17 @@ get_header('supervisor'); ?>
 
 <div class="supervisor-home">
     <!-- Navigation Menu -->
-    <nav class="site-nav supervisor_header_links" aria-label="ראשי">
-        <?php
-            $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
-            if (file_exists($nav_path)) {
-                ob_start();
-                require_once $nav_path;
-                $nav_content = ob_get_clean();
-                // Extract just the links from the navigation file
-                preg_match_all('/<a[^>]*>.*?<\/a>/s', $nav_content, $matches);
-                if (!empty($matches[0])) {
-                    echo implode('', $matches[0]);
-                }
-            }
-        ?>
-    </nav>
+    <?php
+        $nav_path = PLUGIN_ROOT . 'inc/navigation.php';
+        if (file_exists($nav_path)) {
+            require_once $nav_path;
+        }
+    ?>
 
-    <div class="supervisor-content supervisor-two-column-content">
-
-        <?php
-            $sidebar_path =  PLUGIN_ROOT . 'inc/sidebar.php';
-
-            if (file_exists($sidebar_path)) {
-                require_once $sidebar_path;
-            } else {
-                error_log('Sidebar file not found: ' . $sidebar_path);
-            }
-        ?>
-
-        <div>
+    <div class="supervisor-content supervisor-single-column-content">
+        <div class="content-main">
             <?php echo the_content();  ?>
         </div>
-
-        <!-- Vertical Buttons -->
-         
-      
-
     </div>
 
 </div>
